74925b71d2b9abe8a93d24a762c77f80ad0ad401
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-09 10:33:45 -0300

    Instalando o sonner

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { BrowserPage } from "./Routes"
 import { NotesProvider } from "./Context"
+
+// Import toast
+import { Toaster } from 'sonner'
 function App() {
   
   return (
     <NotesProvider>
+      <Toaster/>
       <BrowserPage/>
     </NotesProvider>
   )

552c9daaaf9ad7b1307d92994b2034e3f4db2109
Thomaz Alves <thomazhilario5@gmail.com>
2024-02-14 13:02:01 -0300

    Criando a parte de edição do app notes

--- src/Components/notes-card.tsx
+++ src/Components/notes-card.tsx
@@ -14,6 +14,10 @@ import { db } from '../Services';
 // Import toast
 import { toast } from 'sonner'
 
+// lucide icons
+import { FilePenLine, X  } from 'lucide-react'
+import { useState } from 'react';
+
 // PropsCard
 interface PropsCard{
     date:number,
@@ -25,6 +29,18 @@ export function NotesCard({ date, text, position}:PropsCard){
 
     // state - notes e setNotes
     const { notes, setNotes, id} = useNotes()
+
+    // state - isToEditNote
+    const [isToEditNote, setIsToEditNote] = useState(false)
+
+    // editNote
+    function editNote(){
+        if(isToEditNote === false){
+            setIsToEditNote(true)
+        } else{
+            setIsToEditNote(false)
+        }
+    }
 
     // deleteNotes
     async function deleteNotes(position:number){
@@ -72,13 +88,25 @@ export function NotesCard({ date, text, position}:PropsCard){
                 <Dialog.Content className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
                     
                     {/* Icon de fechar modal */}
-                    <Dialog.Close className=' py-1.5 px-3 absolute right-0 top-0 bg-slate-800 text-white'>x</Dialog.Close>
+                    <Dialog.Close className=' py-1.5 px-3 absolute right-0 top-0 bg-slate-800 text-white'><X size={20}/></Dialog.Close>
 
-                    <div className=' h-[90vh] w-[100vw] sm:h-[60vh] sm:w-[70vw] md:flex flex-col gap-3 p-5 bg-slate-700 rounded-t-md lg:w-[60vw]'>
-                        <h1 className='text-slate-200'>{formatDistanceToNow(date,{addSuffix:true,locale:ptBR})}</h1>
-                        
-                        <p className='text-slate-400'>{text}</p>
-                    </div>
+                    <button className=' py-1.5 px-3 absolute left-0 top-0 bg-slate-800 text-white' onClick={editNote}><FilePenLine size={20} /></button>
+
+                    {isToEditNote 
+                        ? 
+
+                        <div className=' pt-10 h-[90vh] w-[100vw] sm:h-[60vh] sm:w-[70vw] md:flex flex-col gap-3 p-5 bg-slate-700 rounded-t-md lg:w-[60vw]'>
+                            <textarea className='bg-black/10 text-white outline-none resize-none p-1' rows={10} name="myNotes" id="note" autoFocus/>
+                        </div>
+
+                        :
+
+                        <div className=' pt-10 h-[90vh] w-[100vw] sm:h-[60vh] sm:w-[70vw] md:flex flex-col gap-3 p-5 bg-slate-700 rounded-t-md lg:w-[60vw]'>
+                            <h1 className='text-slate-200'>{formatDistanceToNow(date,{addSuffix:true,locale:ptBR})}</h1>
+                            
+                            <p className='text-slate-400'>{text}</p>
+                        </div>
+                    }
 
                     <button className='bg-red-500 w-full rounded-b-md p-2' onClick={() => deleteNotes(position)}>Deseja apagar esta nota ?</button>
                 </Dialog.Content>
